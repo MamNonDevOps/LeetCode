4c4b47c063d1aa0c4728f5d8a7e27b35b142582b
pc-home <pc@home>
2022-12-28 22:31:25 +0700

    doing 1518

---
+++ 1518/main.py
@@ -0,0 +1,9 @@
+class Solution:
+  def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+    drinkBottles = numBottles
+
+    return drinkBottles
+
+if __name__ == "__main__":
+  s = Solution()
+  print(s.numWaterBottles(9, 3))
