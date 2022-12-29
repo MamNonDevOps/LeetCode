5beb52d05f40603280c2c47cf1772d98a3fbe1f3
Khoi Nguyen Van <khoinv@smartosc.com>
2022-12-29 09:24:42 +0700

    doing

--- 1518/main_test.py
+++ 1518/main_test.py
@@ -6,6 +6,7 @@ class TestNumWaterBottles(unittest.TestCase):
 
   def test_numWaterBottles(self):
     self.assertEqual(self.s.numWaterBottles(9, 3), 13, "Should be 13")
+    self.assertEqual(self.s.numWaterBottles(15, 4), 19, "Should be 19")
 
 if __name__ == '__main__':
   unittest.main()
